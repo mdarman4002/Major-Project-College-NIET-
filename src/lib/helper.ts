843013564bdd6af88970a3737dd31c5ddb061cf6
Mohammad Arman <mdarman4002@gmail.com>
2025-04-20 00:29:23 +0530

    Home screen completed

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,19 +1,18 @@
-
 export const MainRoutes = [
-   {
-    label:"Home",
-    href : "/",
-   },
-   {
-    label : "Contact Us",
-    href : "/contact",
-   },
-   {
-    label : "About Us",
-    href : "/about",
+    {
+      label: "Home",
+      href: "/",
     },
     {
-        label : "Services",
-        href : "/services",
+      label: "Contact Us",
+      href: "/contact",
     },
-];
+    {
+      label: "About Us",
+      href: "/about",
+    },
+    {
+      label: "Services",
+      href: "/services",
+    },
+  ];
